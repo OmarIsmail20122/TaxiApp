14db1ef9ab9ff35db19a78b84ecbee9042d903c0
OmarIsmail2019 <OmarIsmail2019@github.com>
2025-03-13 01:48:08 +0200

    Initial Commit

---
+++ TaxiApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TaxiApp
+//
+//  Created by MacBookPro on 13/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TaxiApp/TaxiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxiAppApp.swift
+//  TaxiApp
+//
+//  Created by MacBookPro on 13/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct TaxiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
